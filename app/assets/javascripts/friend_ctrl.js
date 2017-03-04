f3434ba4afbf0f1400fce420875a0557cdd982c0
Wen Zhang <iwenyou@gmail.com>
2017-03-03 22:06:10 -0800

    daily commit

---
+++ app/assets/javascripts/friend_ctrl.js
@@ -0,0 +1,18 @@
+(function() {
+    "use strict";
+
+    angular.module("app").controller("friendCtrl", function($scope, $http) {
+        $scope.message = "hello, world";
+
+        $scope.setup = function() {
+            $http.get("/api/v1/friends.json").then(function(response) {
+                $scope.friends = response.data;
+                console.log($scope.friends);
+                $scope.friendsCount = $scope.friends.length;
+            });
+        };
+
+
+
+    });
+}());
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get "/" => "friends#index"
+
   namespace :api do
 
     namespace :v1 do
@@ -32,5 +34,5 @@ Rails.application.routes.draw do
     end
 
   end
-  
+
 end
